25b908cdb1ad1258dd619b59e7a50cff2dde6c9a
Clement Leung <clement@quandl.com>
2015-06-15 12:19:03 -0400

    download the jdbc file

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,3 +1,7 @@
 include_recipe "teamcity::server"
 include_recipe "teamcity::build_agent"
 include_recipe "teamcity::plugins"
+
+remote_file "#{node['teamcity_server']['data_dir']}/lib/jdbc" do
+  source 'https://jdbc.postgresql.org/download/postgresql-9.4-1201.jdbc41.jar'
+end
